6f523f859dcd916a95d01cdb492a3cd13a513a06
gups <91670399+gsinghg19@users.noreply.github.com>
2022-01-12 15:44:36 +0000

    commit of single article with mui card formatting

--- Desktop/northcoders/frontend/nc-news/nc-news/src/components/SingleArticle.jsx
+++ Desktop/northcoders/frontend/nc-news/nc-news/src/components/SingleArticle.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import { getSingleArticle } from "../utils/api";
 import { useParams } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import React from "react-router-dom";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
@@ -8,6 +15,13 @@ const SingleArticle = () => {
   console.log(article);
   const { article_id } = useParams();
   console.log(article_id);
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    ></Box>
+  );
 
   useEffect(() => {
     setIsLoading(true);
@@ -21,14 +35,21 @@ const SingleArticle = () => {
   return isLoading ? (
     <h1>Loading....</h1>
   ) : (
-    <article className="SingleArticle">
-      <div>
-        <h2>{article.title}</h2>
-        <h4>Posted by: {article.author}</h4>
-        <p>{article.body}</p>
-      </div>
-    </article>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
+          <article className="SingleArticle">
+            <h4>Written by {article.author}</h4>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+          </article>
+          <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="large">Expand</Button>
+      </CardActions>
+    </Card>
   );
 };
-
 export default SingleArticle;
