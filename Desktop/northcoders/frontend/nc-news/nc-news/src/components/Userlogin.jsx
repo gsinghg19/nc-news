686fff3e1f2672857c4b2b73f0cece8dd7e56842
gups <91670399+gsinghg19@users.noreply.github.com>
2022-01-14 17:14:40 +0000

    added dropdown list foir userlogin

--- Desktop/northcoders/frontend/nc-news/nc-news/src/components/Userlogin.jsx
+++ Desktop/northcoders/frontend/nc-news/nc-news/src/components/Userlogin.jsx
@@ -1,10 +1,82 @@
-const UserLogin = ({ username }) => {
-  console.log(username);
-  return (
-    <h2>
-      <p className="UserLogin">Logged in as: {`users.${username}`}</p>
-    </h2>
-  );
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
 };
 
-export default UserLogin;
+const usernames = [
+  "tickle122",
+  "grumpy19",
+  "happyamy2016",
+  "cooljmessy",
+  "weegembump",
+  "jessjelly",
+];
+
+function getStyles(username, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(username) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function UserLogin() {
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  return (
+    <div>
+      {" "}
+      <h2>
+        <p className="UserLogin">Logged in as: </p>
+      </h2>
+      <FormControl sx={{ m: 3, width: 300 }}>
+        <InputLabel id="multiple-username-label">username</InputLabel>
+        <Select
+          labelId="multiple-username-label"
+          id="multiple-username"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="username" />}
+          MenuProps={MenuProps}
+        >
+          {usernames.map((username) => (
+            <MenuItem
+              key={username}
+              value={username}
+              style={getStyles(username, personName, theme)}
+            >
+              {username}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
